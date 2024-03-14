1f8317804a5afef962cda496efc90fd61dde2c6f
Gajraj Singh Bhati <110357099+gsbgajraj@users.noreply.github.com>
2024-03-15 00:05:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,13 @@ const EmployeeModel = require("./models/Employee")
 
 const app = express();
 app.use(express.json())
-app.use(cors()); 
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST","GET"],
+        credentials: true
+    }
+)); 
 
 mongoose.connect("mongodb://127.0.0.1:27017/employee")
 
